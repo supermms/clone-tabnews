dcd45b8fd622d9695c8aa1f63edf007853cac376
Matheus Moreno Sá <mms.matheus1@gmail.com>
2025-11-21 23:21:36 +0000

    Alterada a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Nenem, amo você. Se vc me ama, canta piruliru beibi.</h1>
+    return <h1> Nenem, amo você. Se vc me ama, manda audio cantando piruliru beibi.</h1>
 }
 
 export default Home;
