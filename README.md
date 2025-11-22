3f0486a4a01e2df4fb2d4240348510dd24bbee07
Matheus Moreno Sá <mms.matheus1@gmail.com>
2025-11-22 19:26:38 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto end-to-end de clone do https://www.tabnews.com.br. Base do curso https://www.curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1> Nenem, amo você. Se vc me ama, manda audio cantando piruliru beibi.</h1>
+  return (
+    <h1>Nenem, amo você. Se vc me ama, manda audio cantando piruliru beibi.</h1>
+  );
 }
 
 export default Home;
